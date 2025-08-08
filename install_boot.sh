1c3bd21b9365efc7ab04f14150d8362cb4a9abaa
habib <habibadeku@gmail.com>
2025-08-08 20:33:11 +0200

    fix: boot script updates

--- install_boot.sh
+++ install_boot.sh
@@ -6,13 +6,17 @@ cat > ~/.termux/boot/start_naistro.sh << 'EOF'
 #!/bin/bash
 am start -n com.termux/com.termux.app.TermuxActivity
 termux-wake-lock
-sleep 5
-cd ~/naistro-player-box-dist
-clear
-python3 dist/naistro-player-box.pyz
 EOF
 
 # Set permissions
 chmod +x ~/.termux/boot/start_naistro.sh
+
+cat > ~/.bashrc << 'EOF'
+#!/bin/bash
+sleep 2
+cd ~/naistro-player-box-dist
+clear
+python3 dist/naistro-player-box.pyz
+EOF
 
 echo "Setup complete! Reboot to test."
